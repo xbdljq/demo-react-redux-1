a7459f77e30c5d09cefe8af9c285e4aa56d99824
liangjq <2044527225@qq.com>
2019-05-30 17:32:24 +0800

    react-redux-demo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import React, {Component} from 'react';
 import './App.css';
+import { connect } from "react-redux";
 
 class App extends Component {
-  add1(){
-      this.props.onAdd1()
-  }
-  add2(){
-    this.props.onAdd2()
-  }
-  add3(){
-    this.props.onAdd3()
-  }
-  add4(){
-    this.props.onAdd4()
-  }
+  // add1(){
+  //     this.props.onAdd1()
+  // }
+  // add2(){
+  //   this.props.onAdd2()
+  // }
+  // add3(){
+  //   this.props.onAdd3()
+  // }
+  // add4(){
+  //   this.props.onAdd4()
+  // }
   render(){
     return (
       <div className="App">
           <div>
-              你点击了<span id="value">{this.props.value}</span>次
+              你点击了<span id="value">
+                {/* //{this.props.value} */}
+                {this.props.n}
+              </span>次
             <div>
-              <button id="add1" onClick={() => this.add1()}>+1</button>
+              {/* <button id="add1" onClick={() => this.add1()}>+1</button>
               <button id="add2" onClick={() => this.add2()}>+2</button>
               <button id="minus1" >-1</button>
               <button id="add1IfOdd" onClick={() => this.add3()}>如果是单数就+1</button>
-                  <button id="add1AfterSec" onClick={() => this.add4()}>2秒钟后+1</button>
+              <button id="add1AfterSec" onClick={() => this.add4()}>2秒钟后+1</button> */}
+              <button id="add1" onClick={()=> this.props.add1()}>+1</button>
             </div>
           </div>
       </div>
@@ -32,5 +37,17 @@ class App extends Component {
   }
   
 }
+function mapStateToProps(state){
+  return {
+    n: state.n
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    add1: ()=> dispatch({type:'add', payload: 1})
+  }
+}
 
-export default App;
+
+// export default App;
+export default connect(mapStateToProps,mapDispatchToProps)(App);
