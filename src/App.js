65a610b0139c5a401483daa68b8476b966b84135
liangjq <2044527225@qq.com>
2019-05-30 16:24:24 +0800

    初次代码

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  add1(){
+      this.props.onAdd1()
+  }
+  add2(){
+    this.props.onAdd2()
+  }
+  add3(){
+    this.props.onAdd3()
+  }
+  add4(){
+    this.props.onAdd4()
+  }
+  render(){
+    return (
+      <div className="App">
+          <div>
+              你点击了<span id="value">{this.props.value}</span>次
+            <div>
+              <button id="add1" onClick={() => this.add1()}>+1</button>
+              <button id="add2" onClick={() => this.add2()}>+2</button>
+              <button id="minus1" >-1</button>
+              <button id="add1IfOdd" onClick={() => this.add3()}>如果是单数就+1</button>
+                  <button id="add1AfterSec" onClick={() => this.add4()}>2秒钟后+1</button>
+            </div>
+          </div>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
